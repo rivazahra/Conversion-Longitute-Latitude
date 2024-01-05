c910ac730c8bcc3b34c2a6b48bd0b64856f1a4b9
rivazahra <riva.zahra88@gmail.com>
2024-01-05 23:18:38 +0700

    finish

--- src/components/FormKonversi.jsx
+++ src/components/FormKonversi.jsx
@@ -1,110 +1,86 @@
 import { useState } from "react";
 import { transformDmsToDd, transformDdToDms } from "../logicConvert";
 
-
-
 const FormKonversi = ({ onAddMarker }) => {
-	const [conversionType, setConversionType] = useState("DMS to DD");
-	const [latitude, setLatitude] = useState("");
-	const [longitude, setLongitude] = useState("");
-	const [output, setOutput] = useState({ lat: "", lon: "" });
+  const [conversionType, setConversionType] = useState("DMS to DD");
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [output, setOutput] = useState({ lat: "", lon: "" });
 
-	const handleConvert = () => {
-		if (latitude && longitude) {
-			const latConverter = conversionType === "DMS to DD" ? transformDmsToDd : parseFloat;
-			const lonConverter = conversionType === "DMS to DD" ? transformDmsToDd : parseFloat;
+  const handleConvert = () => {
+    if (latitude && longitude) {
+      const latConverter = conversionType === "DMS to DD" ? transformDmsToDd : parseFloat;
+      const lonConverter = conversionType === "DMS to DD" ? transformDmsToDd : parseFloat;
 
-			const latValue = latConverter(latitude);
-			const lonValue = lonConverter(longitude);
+      const latValue = latConverter(latitude);
+      const lonValue = lonConverter(longitude);
 
-			setOutput({ lat: `${latValue}°`, lon: `${lonValue}°` });
-		}
-	};
+      setOutput({ lat: `${latValue}°`, lon: `${lonValue}°` });
+    }
+  };
 
-	const handleMap = () => {
-		if (output) {
-			onAddMarker(parseFloat(output.lat), parseFloat(output.lon));
-		}
-	};
+  const handleMap = () => {
+    if (output) {
+      onAddMarker(parseFloat(output.lat), parseFloat(output.lon));
+    }
+  };
 
-	return (
-		<div className="absolute top-0 mx-8 my-24  bg-white p-6 rounded-lg shadow-lg border border-gray-500">
-			<h2 className="text-lg font-semibold mb-4 text-amber-700"></h2>
-			<div className="flex space-x-4 mb-4">
-				<button
-					onClick={() => setConversionType("DMS to DD")}
-					className={`px-4 py-2 rounded w-full h-12 ${conversionType === "DMS to DD" ? "bg-amber-800 text-white" : "bg-amber-600 text-white"}`}
-				>
-					DMS to DD
-				</button>
-				<button
-					onClick={() => setConversionType("DD to DMS")}
-					className={`px-4 py-2 rounded w-full h-12 ${conversionType === "DD to DMS" ? "bg-amber-800 text-white" : "bg-amber-600 text-white"}`}
-				>
-					DD to DMS
-				</button>
-			</div>
-			<div className="flex flex-col mb-4">
-	<label htmlFor="latitude" className="mb-2 text-gray-800">
-		Latitude 
-		
-	</label>
-	<input
-		type="text"
-		id="latitude"
-		value={latitude}
-		onChange={(e) => setLatitude(e.target.value)}
-		placeholder={conversionType === "DMS to DD" ? "e.g. 90 23 52 N" : "e.g. 90.221"}
-		className="border border-gray-400 rounded p-2 w-full h-10"
-	/>
-</div>
-<div className="flex flex-col mb-4">
-	<label htmlFor="longitude" className="mb-2 text-gray-800">
-		Longitude 
-		
-	</label>
-	<input
-		type="text"
-		id="longitude"
-		value={longitude}
-		onChange={(e) => setLongitude(e.target.value)}
-		placeholder={conversionType === "DMS to DD" ? "e.g. 54 78 96 W" : "e.g. -84.221"}
-		className="border border-gray-400 rounded p-2 w-full h-12"
-	/>
-</div>
-<button onClick={handleConvert} className="bg-amber-900 text-white px-4 py-2 rounded mb-4 w-full h-12">
-	Konversi
-</button>
-<div className="flex flex-col mb-4">
-	<label htmlFor="outputLat" className="mb-2 text-gray-800">
-		Latitude:
-	</label>
-	<input
-		type="text"
-		id="outputLat"
-		value={output.lat}
-		readOnly
-		className="border border-gray-400 rounded p-2 w-full h-12"
-	/>
-</div>
-<div className="flex flex-col mb-4">
-	<label htmlFor="outputLon" className="mb-2 text-gray-800">
-		Longitude:
-	</label>
-	<input
-		type="text"
-		id="outputLon"
-		value={output.lon}
-		readOnly
-		className="border border-gray-400 rounded p-2 w-full h-12"
-	/>
-</div>
-<button onClick={handleMap} className="bg-amber-600 text-white px-4 py-2 rounded w-full h-12">
-	Tambahkan ke map
-</button>
+  return (
+    <div className="absolute  top-0 ml-96 w-1/3 mt-3  bg-white p-2 rounded-lg shadow-lg border border-gray-500">
+      <h2 className="text-lg font-semibold mb-4 text-amber-300 ">
+	  <hr
+      className={`transition-transform transform border-b-2 border-amber-300 ${
+        conversionType === "DMS to DD" ? "translate-x-0" : "translate-x-full"
+      } absolute bottom-0 left-0 w-full`}
+    />
+  </h2>
+      <div className="flex space-x-4 mb-4 bg-amber-700">
+        <button onClick={() => setConversionType("DMS to DD")} className={`px-4 py-2 rounded w-full h-12 ${conversionType === "DMS to DD" ? "bg-amber-800 text-white" : " text-white"}`}>
+          DMS to DD
+        </button>
+        <button onClick={() => setConversionType("DD to DMS")} className={`px-4 py-2 rounded w-full h-12 ${conversionType === "DD to DMS" ? "bg-amber-800 text-white" : " text-white"}`}>
+          DD to DMS
+        </button>
+      </div>
+	  <div className="flex gap-2">
 
-		</div>
-	);
+      <div className="flex flex-col mb-4">
+        <label htmlFor="latitude" className="mb-2 text-gray-800">
+          Latitude
+        </label>
+        <input type="text" id="latitude" value={latitude} onChange={(e) => setLatitude(e.target.value)} className="border border-gray-400 rounded p-2  w-full " />
+      </div>
+      <div className="flex flex-col mb-4">
+        <label htmlFor="longitude" className="mb-2 text-gray-800">
+          Longitude
+        </label>
+        <input type="text" id="longitude" value={longitude} onChange={(e) => setLongitude(e.target.value)} className="border border-gray-400 rounded p-2  w-full " />
+      </div>
+	  </div>
+      <button onClick={handleConvert} className="bg-amber-900 text-white px-4 py-2 rounded mb-4 w-full ">
+        Konversi
+      </button>
+	  
+	  <div className="flex gap-2">
+
+      <div className="flex flex-col mb-4">
+        <label htmlFor="outputLat" className="mb-2 text-gray-800">
+          Latitude:
+        </label>
+        <input type="text" id="outputLat" value={output.lat} readOnly className="border border-gray-400 rounded p-2 w-full " />
+      </div>
+      <div className="flex flex-col mb-4">
+        <label htmlFor="outputLon" className="mb-2 text-gray-800">
+          Longitude:
+        </label>
+        <input type="text" id="outputLon" value={output.lon} readOnly className="border border-gray-400 rounded p-2 w-full" />
+	  </div>
+      </div>
+      <button onClick={handleMap} className="bg-amber-600 text-white px-4 py-2 rounded w-full h-12">
+        Tambahkan ke map
+      </button>
+    </div>
+  );
 };
 
 export default FormKonversi;
--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import "ol/ol.css";
 import Map from "ol/Map";
 import View from "ol/View";
-import Select from 'ol/interaction/Select';
+import Select from "ol/interaction/Select";
 import TileLayer from "ol/layer/Tile";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
@@ -16,98 +16,96 @@ import Overlay from "ol/Overlay.js";
 import { toLonLat } from "ol/proj";
 
 const MyMap = ({ onAddMarker, titikTarget }) => {
-	const mapRef = useRef(null);
-	const [vectorLayerSource] = useState(new VectorSource());
-  
-	useEffect(() => {
-	  if (!mapRef.current) {
-		mapRef.current = new Map({
-		  layers: [
-			new TileLayer({
-			  source: new OSM(),
-			}),
-			new VectorLayer({
-			  source: vectorLayerSource,
-			}),
-		  ],
-		  target: 'map',
-		  view: new View({
-			center: [0, 0],
-			zoom: 2,
-		  }),
-		});
-		const selectInteraction = new Select();
-mapRef.current.addInteraction(selectInteraction);
+  const mapRef = useRef(null);
+  const [vectorLayerSource] = useState(new VectorSource());
 
-selectInteraction.on('select', (event) => {
-	// ... kode untuk mengubah gaya icon ...
-	const lonLat = toLonLat(event.coordinate);
-	onAddMarker(lonLat[1], lonLat[0]);
-  });
-		mapRef.current.on("click", function (event) {
-			const lonLat = toLonLat(event.coordinate);
-			onAddMarker(lonLat[1], lonLat[0]);
-		});
-	}
+  useEffect(() => {
+    if (!mapRef.current) {
+      mapRef.current = new Map({
+        layers: [
+          new TileLayer({
+            source: new OSM(),
+          }),
+          new VectorLayer({
+            source: vectorLayerSource,
+          }),
+        ],
+        target: "map",
+        view: new View({
+          center: [0, 0],
+          zoom: 2,
+        }),
+      });
+      const selectInteraction = new Select();
+      mapRef.current.addInteraction(selectInteraction);
 
-	if (titikTarget) {
-		const [long, lat] = titikTarget;
-		const view = mapRef.current.getView();
-		view.animate({
-		  center: fromLonLat([long, lat]),
-		  duration: 1000,
-		  zoom: 7,
-		});
-  
-		const pointFeature = new Feature({
-		  geometry: new Point(fromLonLat([long, lat])),
-		});
-		pointFeature.setStyle(
-			new Style({
-				image: new Icon({
-					color: 'rgba(0, 255, 0, 0.7)', // Ubah warna icon saat dipilih
-					crossOrigin: 'anonymous',
-					src: 'https://openlayers.org/en/latest/examples/data/bigdot.png',
-					scale: 0.2,
-				}),
-		  })
-		);
-		vectorLayerSource.addFeature(pointFeature);
-	  }
-		mapRef.current.on('click', function (evt) {
-		  const coordinate = evt.coordinate;
-		  const hdms = toStringHDMS(toLonLat(coordinate));
-		  const popup = new Overlay({
-			      element: document.getElementById("popup"),
-			    });
-			    mapRef.current.addOverlay(popup);
-		  // Assuming you have a 'popup' and 'element' defined somewhere in your code
-		  const element = popup.getElement();
-		  popup.setPosition(coordinate);
-		  let popover = bootstrap.Popover.getInstance(element);
-		  if (popover) {
-			popover.dispose();
-		  }
-		  popover = new bootstrap.Popover(element, {
-			animation: false,
-			container: element,
-			content: "<p>The location you clicked was:</p><code>" + hdms + '</code>',
-			html: true,
-			
-			placement: 'top',
-			title: 'Welcome to OpenLayers',
-		  });
-		  popover.show();
-		});
-	}, [onAddMarker, titikTarget, vectorLayerSource]);
-  
-	return (
-	  <div>
-		<div id="map" className="h-screen border-2 bg-gray-500"></div>
-	  </div>
-	);
-  };
-  
-  export default MyMap;
+      selectInteraction.on("select", (event) => {
+        // ... kode untuk mengubah gaya icon ...
+        const lonLat = toLonLat(event.coordinate);
+        onAddMarker(lonLat[1], lonLat[0]);
+      });
+      mapRef.current.on("click", function (event) {
+        const lonLat = toLonLat(event.coordinate);
+        onAddMarker(lonLat[1], lonLat[0]);
+      });
+    }
 
+    if (titikTarget) {
+      const [long, lat] = titikTarget;
+      const view = mapRef.current.getView();
+      view.animate({
+        center: fromLonLat([long, lat]),
+        duration: 1000,
+        zoom: 7,
+      });
 
+      const pointFeature = new Feature({
+        geometry: new Point(fromLonLat([long, lat])),
+      });
+      pointFeature.setStyle(
+        new Style({
+          image: new Icon({
+            color: "rgba(0, 255, 0, 0.7)", // Ubah warna icon saat dipilih
+            crossOrigin: "anonymous",
+            src: "https://openlayers.org/en/latest/examples/data/bigdot.png",
+            scale: 0.2,
+          }),
+        })
+      );
+      vectorLayerSource.addFeature(pointFeature);
+    }
+    mapRef.current.on("click", function (evt) {
+      const coordinate = evt.coordinate;
+      const hdms = toStringHDMS(toLonLat(coordinate));
+      const popup = new Overlay({
+        element: document.getElementById("popup"),
+      });
+      mapRef.current.addOverlay(popup);
+      // Assuming you have a 'popup' and 'element' defined somewhere in your code
+      const element = popup.getElement();
+      popup.setPosition(coordinate);
+      let popover = bootstrap.Popover.getInstance(element);
+      if (popover) {
+        popover.dispose();
+      }
+      popover = new bootstrap.Popover(element, {
+        animation: false,
+        container: element,
+        content: "<p>The location you clicked was:</p><code>" + hdms + "</code>",
+        html: true,
+
+        placement: "top",
+        title: "Welcome to OpenLayers",
+      });
+      popover.show();
+    });
+  }, [onAddMarker, titikTarget, vectorLayerSource]);
+
+  return (
+    <div>
+      <div id="map" className="h-screen border-2 bg-gray-500"></div>
+    </div>
+  );
+};
+
+export default MyMap;
